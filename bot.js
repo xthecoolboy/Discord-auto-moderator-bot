f0f44cb99824011a05d857a814e4efbba680581c
warhammercasey <warhammercasey@gmail.com>
2018-03-31 19:50:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,4 +2,17 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
 
+var raidChannel;
+var crucibleChannel;
+var pveChannel;
+var questionsChannel;
+
+client.on('ready', () => {
+	console.log("I am ready!");
+});
+
+client.on('message', message => {
+	
+});
+
 client.login(process.env.BOT_TOKEN);
