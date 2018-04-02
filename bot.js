358c7b61ecb19e1332093aa824d6f12c39d545c3
warhammercasey <warhammercasey@gmail.com>
2018-04-02 17:52:21 -0600

    t

--- bot.js
+++ bot.js
@@ -61,13 +61,15 @@ client.on('message', message => {
 						.catch(console.error);
 					break;
 				case 'crucible':
-					crucibleReqs.push(requirement);
+				    crucibleReqs.push(requirement);
+				    setupReq("crucible");
 					message.channel.send('Added requirement to crucible: ' + requirement)
 						.then(message => console.log(`Sent message: ${message.content}`))
 						.catch(console.error);
 					break;
 				case 'pve':
-					pveReqs.push(requirement);
+				    pveReqs.push(requirement);
+				    setupReq("pve");
 					message.channel.send('Added requirement to pve: ' + requirement)
 						.then(message => console.log(`Sent message: ${message.content}`))
 						.catch(console.error);
@@ -76,6 +78,7 @@ client.on('message', message => {
 					raidReqs.push(requirement);
 					crucibleReqs.push(requirement);
 					pveReqs.push(requirement);
+					setupReq("all");
 					message.channel.send('Added requirement to all: ' + requirement)
 						.then(message => console.log(`Sent message: ${message.content}`))
 						.catch(console.error);
@@ -322,6 +325,142 @@ function setupReq(id) {
                 if (raidor[raidReqs.length - 1][i].includes("$")) {
                     for (a = 0; a <= raidor[raidReqs.length - 1][i].split("$").length - 1; a++) {
                         raidWild[raidReqs.length - 1][i][a] = getPosition(raidReqs[raidReqs.length - 1], '$', a+1);
+                    }
+                }
+            }
+            break;
+        case 'crucible':
+            if (crucibleReqs[crucibleReqs.length - 1].includes("||")) {
+                for (i = 0; i <= crucibleReqs[crucibleReqs.length - 1].split("||").length - 1; i++) {
+                    crucibleor[crucibleReqs.length - 1][i] = crucibleReqs[crucibleReqs.length - 1].split("||")[i];
+                }
+            } else {
+                crucibleor[crucibleReqs.length - 1][0] = crucibleReqs[crucibleReqs.length - 1];
+            }
+            for (i = 0; i < crucibleor[crucibleReqs.length - 1].length; i++) {
+                if (crucibleor[crucibleReqs.length - 1][i].charAt(0) == '~') {
+                    if (crucibleor[crucibleReqs.length - 1][i].slice(-1) == '~') {
+                        crucibleId[crucibleReqs.length - 1][i] = "both";
+                    } else {
+                        crucibleId[crucibleReqs.length - 1][i] = "front";
+                    }
+                } else if (crucibleor[crucibleReqs.length - 1][i].slice(-1) == '~') {
+                    crucibleId[crucibleReqs.length - 1][i] = "end";
+                } else {
+                    crucibleId[crucibleReqs.length - 1][i] = "none";
+                }
+                crucibleWild[crucibleReqs.length - 1][i] = [];
+                if (crucibleor[crucibleReqs.length - 1][i].includes("$")) {
+                    for (a = 0; a <= crucibleor[crucibleReqs.length - 1][i].split("$").length - 1; a++) {
+                        crucibleWild[crucibleReqs.length - 1][i][a] = getPosition(crucibleReqs[crucibleReqs.length - 1], '$', a + 1);
+                    }
+                }
+            }
+            break;
+        case 'pve':
+            if (pveReqs[pveReqs.length - 1].includes("||")) {
+                for (i = 0; i <= pveReqs[pveReqs.length - 1].split("||").length - 1; i++) {
+                    pveor[pveReqs.length - 1][i] = pveReqs[pveReqs.length - 1].split("||")[i];
+                }
+            } else {
+                pveor[pveReqs.length - 1][0] = pveReqs[pveReqs.length - 1];
+            }
+            for (i = 0; i < pveor[pveReqs.length - 1].length; i++) {
+                if (pveor[pveReqs.length - 1][i].charAt(0) == '~') {
+                    if (pveor[pveReqs.length - 1][i].slice(-1) == '~') {
+                        pveId[pveReqs.length - 1][i] = "both";
+                    } else {
+                        pveId[pveReqs.length - 1][i] = "front";
+                    }
+                } else if (pveor[pveReqs.length - 1][i].slice(-1) == '~') {
+                    pveId[pveReqs.length - 1][i] = "end";
+                } else {
+                    pveId[pveReqs.length - 1][i] = "none";
+                }
+                pveWild[pveReqs.length - 1][i] = [];
+                if (pveor[pveReqs.length - 1][i].includes("$")) {
+                    for (a = 0; a <= pveor[pveReqs.length - 1][i].split("$").length - 1; a++) {
+                        pveWild[pveReqs.length - 1][i][a] = getPosition(pveReqs[pveReqs.length - 1], '$', a + 1);
+                    }
+                }
+            }
+            break;
+        case 'all':
+            if (raidReqs[raidReqs.length - 1].includes("||")) {
+                for (i = 0; i <= raidReqs[raidReqs.length - 1].split("||").length - 1; i++) {
+                    raidor[raidReqs.length - 1][i] = raidReqs[raidReqs.length - 1].split("||")[i];
+                }
+            } else {
+                raidor[raidReqs.length - 1][0] = raidReqs[raidReqs.length - 1];
+            }
+            for (i = 0; i < raidor[raidReqs.length - 1].length; i++) {
+                if (raidor[raidReqs.length - 1][i].charAt(0) == '~') {
+                    if (raidor[raidReqs.length - 1][i].slice(-1) == '~') {
+                        raidId[raidReqs.length - 1][i] = "both";
+                    } else {
+                        raidId[raidReqs.length - 1][i] = "front";
+                    }
+                } else if (raidor[raidReqs.length - 1][i].slice(-1) == '~') {
+                    raidId[raidReqs.length - 1][i] = "end";
+                } else {
+                    raidId[raidReqs.length - 1][i] = "none";
+                }
+                raidWild[raidReqs.length - 1][i] = [];
+                if (raidor[raidReqs.length - 1][i].includes("$")) {
+                    for (a = 0; a <= raidor[raidReqs.length - 1][i].split("$").length - 1; a++) {
+                        raidWild[raidReqs.length - 1][i][a] = getPosition(raidReqs[raidReqs.length - 1], '$', a + 1);
+                    }
+                }
+            }
+            if (crucibleReqs[crucibleReqs.length - 1].includes("||")) {
+                for (i = 0; i <= crucibleReqs[crucibleReqs.length - 1].split("||").length - 1; i++) {
+                    crucibleor[crucibleReqs.length - 1][i] = crucibleReqs[crucibleReqs.length - 1].split("||")[i];
+                }
+            } else {
+                crucibleor[crucibleReqs.length - 1][0] = crucibleReqs[crucibleReqs.length - 1];
+            }
+            for (i = 0; i < crucibleor[crucibleReqs.length - 1].length; i++) {
+                if (crucibleor[crucibleReqs.length - 1][i].charAt(0) == '~') {
+                    if (crucibleor[crucibleReqs.length - 1][i].slice(-1) == '~') {
+                        crucibleId[crucibleReqs.length - 1][i] = "both";
+                    } else {
+                        crucibleId[crucibleReqs.length - 1][i] = "front";
+                    }
+                } else if (crucibleor[crucibleReqs.length - 1][i].slice(-1) == '~') {
+                    crucibleId[crucibleReqs.length - 1][i] = "end";
+                } else {
+                    crucibleId[crucibleReqs.length - 1][i] = "none";
+                }
+                crucibleWild[crucibleReqs.length - 1][i] = [];
+                if (crucibleor[crucibleReqs.length - 1][i].includes("$")) {
+                    for (a = 0; a <= crucibleor[crucibleReqs.length - 1][i].split("$").length - 1; a++) {
+                        crucibleWild[crucibleReqs.length - 1][i][a] = getPosition(crucibleReqs[crucibleReqs.length - 1], '$', a + 1);
+                    }
+                }
+            }
+            if (pveReqs[pveReqs.length - 1].includes("||")) {
+                for (i = 0; i <= pveReqs[pveReqs.length - 1].split("||").length - 1; i++) {
+                    pveor[pveReqs.length - 1][i] = pveReqs[pveReqs.length - 1].split("||")[i];
+                }
+            } else {
+                pveor[pveReqs.length - 1][0] = pveReqs[pveReqs.length - 1];
+            }
+            for (i = 0; i < pveor[pveReqs.length - 1].length; i++) {
+                if (pveor[pveReqs.length - 1][i].charAt(0) == '~') {
+                    if (pveor[pveReqs.length - 1][i].slice(-1) == '~') {
+                        pveId[pveReqs.length - 1][i] = "both";
+                    } else {
+                        pveId[pveReqs.length - 1][i] = "front";
+                    }
+                } else if (pveor[pveReqs.length - 1][i].slice(-1) == '~') {
+                    pveId[pveReqs.length - 1][i] = "end";
+                } else {
+                    pveId[pveReqs.length - 1][i] = "none";
+                }
+                pveWild[pveReqs.length - 1][i] = [];
+                if (pveor[pveReqs.length - 1][i].includes("$")) {
+                    for (a = 0; a <= pveor[pveReqs.length - 1][i].split("$").length - 1; a++) {
+                        pveWild[pveReqs.length - 1][i][a] = getPosition(pveReqs[pveReqs.length - 1], '$', a + 1);
                     }
                 }
             }
