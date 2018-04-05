b5539d211e1b444b7163324ad1e5c0d77b034c12
warhammercasey <warhammercasey@gmail.com>
2018-04-05 17:25:30 -0600

    t

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-const config = require('./config');
+const Config = require('./config');
+const config = new Config();
 
 var raidChannel;
 var crucibleChannel;
