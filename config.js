66a162e2b10e5c5157de4137b52b9725c4fdc8f2
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-17 14:49:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.defaultRaidReqs = [new RegExp('LF[1-5]M|LFG', 'i'), new RegExp('prestige|
 config.defaultCrucibleReqs = [new RegExp('LF[1-4]M|LFG', 'i'), new RegExp('trials|crucible|pvp|quickplay|quick|anything', 'i'), new RegExp('https://discord.gg/|CC[1-5]', 'i')];
 config.defaultPveReqs = [new RegExp('LF[1-5]M|LFG', 'i'), new RegExp('prestige|prest|prest.|normal|anything', 'i'), new RegExp('nf|nightfall|anything|rat king|public event|', 'i'), new RegExp('https://discord.gg/|CS[1-4]', 'i')];
 
-config.defaultAdminRoles = ['LEADERSHIP'];
+config.defaultAdminRoles = ['lol'];
 
 config.defaultHereRequired = false;
 
